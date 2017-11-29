7a7cba0341ff8f1e33c4f56a037f56a4516d4f66
vpranathi17 <33180562+vpranathi17@users.noreply.github.com>
2017-11-28 21:05:41 -0500

    Add files via upload

--- serverTest.java
+++ serverTest.java
@@ -7,17 +7,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.InvalidKeyException;
@@ -67,6 +62,7 @@ class Handler extends JFrame implements Runnable {
     DataInputStream dis1;
     DataInputStream dis2;
     DataInputStream dis3;
+    DataInputStream dis4;
 	static java.sql.Connection conn = null;
 	static Statement stmt;
 	KeyPair serverKey;
@@ -151,8 +147,8 @@ class Handler extends JFrame implements Runnable {
 		Cipher cipher = Cipher.getInstance("RSA");
 		cipher.init(Cipher.DECRYPT_MODE, serverKey.getPrivate());
 		String username = new String(cipher.doFinal(cipherText));
-
-    	dis2 = new DataInputStream(connection.getInputStream());
+		
+		dis2 = new DataInputStream(connection.getInputStream());
 		int length1 = dis2.readInt();
 		
 		byte[] cipherText1 = null;
@@ -160,10 +156,12 @@ class Handler extends JFrame implements Runnable {
 			cipherText1 = new byte[length1];
 		    dis2.readFully(cipherText1, 0, cipherText1.length); 
 		}
-	
+		System.out.println(cipherText1);
 		Cipher cipher1 = Cipher.getInstance("RSA");
 		cipher1.init(Cipher.DECRYPT_MODE, serverKey.getPrivate());
-		String password = new String(cipher1.doFinal(cipherText1));
+		byte[] pass = cipher1.doFinal(cipherText1);
+    	System.out.println(pass);
+		String password = new String(pass);
 		
     	dis3 = new DataInputStream(connection.getInputStream());
 		int length2 = dis3.readInt();
@@ -176,24 +174,42 @@ class Handler extends JFrame implements Runnable {
 		
 		Cipher cipher2 = Cipher.getInstance("RSA");
 		cipher2.init(Cipher.DECRYPT_MODE, serverKey.getPrivate());
-		String info = new String(cipher2.doFinal(cipherText2));
+		String email = new String(cipher2.doFinal(cipherText2));
+		
+    	dis4 = new DataInputStream(connection.getInputStream());
+		int length3 = dis4.readInt();
+		
+		byte[] cipherText3 = null;
+		if(length3>0) {
+			cipherText3 = new byte[length3];
+		    dis4.readFully(cipherText3, 0, cipherText3.length); 
+		}
+		
+		Cipher cipher3 = Cipher.getInstance("RSA");
+		cipher3.init(Cipher.DECRYPT_MODE, serverKey.getPrivate());
+		String info = new String(cipher3.doFinal(cipherText3));
     	
     	ObjectInputStream ois = new ObjectInputStream(connection.getInputStream());
     	PublicKey clientPublicKey = (PublicKey) ois.readObject();
-    	
+
     	System.out.println(username);
     	System.out.println(password);
+    	System.out.println(email);
     	System.out.println(info);
     	System.out.println(clientPublicKey);
-    	databaseCheck(username,password,clientPublicKey,info);
+    	InetAddress inet = connection.getInetAddress();
+    	int portNum = connection.getPort();
+    	System.out.println(inet+":"+ portNum);
+    	databaseCheck(username,password,clientPublicKey,info,inet,portNum,email);
     	}
   
-  	private void databaseCheck(String username, String password, PublicKey clientPublicKey, String info) throws SQLException, IOException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException{
+  	private void databaseCheck(String username, String password, PublicKey clientPublicKey, String info, InetAddress inet, int portNum, String email) throws SQLException, IOException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException{
   		String msg = "";
 		DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
   		String url = ("jdbc:sqlite:/Users/PranathiVasireddy/Desktop/sqlite/test.db");	
 		conn = DriverManager.getConnection(url);
 		stmt = conn.createStatement();
+		//System.out.println("EMAIL_ID ='"+email+"'");
 		String sql = ("SELECT USERNAME FROM LOGIN WHERE USERNAME ='"+username+"'");
 		ResultSet rs = stmt.executeQuery(sql);
 		if (rs.next())
@@ -203,12 +219,15 @@ class Handler extends JFrame implements Runnable {
         	if (res.next() && info.equals("Sign In")) {//username exists, password exists, Sign in
         		msg = "OK";
         		dos.writeUTF(msg);
+        		String sq = ("UPDATE LOGIN SET ADDRESS = '"+inet+"', PORT = '"+portNum+"' WHERE USERNAME = '"+username+"'");
+        		stmt.execute(sq);
         		connectionCheck();
         	}
         	else{
         		if (info.equals("Sign Up")){//username exists, password doesn't exists, Sign up
         			//username exists, password exists, Sign up
         		msg = "Exists";
+        		System.out.println("From username,password");
         		}
         		else{// username exists, password doesn't exists, Sign in
         			msg = "Not Cool";
@@ -217,14 +236,23 @@ class Handler extends JFrame implements Runnable {
         	}
 		}
 		else{//username doesn't exists
-			if (info.equals("Sign Up")){
-			    String seq = ("INSERT INTO LOGIN (USERNAME,PASSWORD,PUBLIC_KEY) " +
-	                       "VALUES('"+username+"','"+password+"','"+clientPublicKey+"')"); 
-			    stmt.execute(seq);
-			    msg = "Inserted";
+			String seq = ("SELECT EMAIL_ID FROM LOGIN WHERE EMAIL_ID='"+email+"'");
+			ResultSet res = stmt.executeQuery(seq);//returns false even if data exists
+			if (!res.next()){//email doesn't exists
+				if (info.equals("Sign Up") ){//Sign up and email is new
+				    String seql = ("INSERT INTO LOGIN (USERNAME,PASSWORD,PUBLIC_KEY,ADDRESS,PORT,EMAIL_ID) " +
+		                       "VALUES('"+username+"','"+password+"','"+clientPublicKey+"','"+inet+"','"+portNum+"','"+email+"')"); 
+				    stmt.execute(seql);
+				    msg = "Inserted";
+				}
+				else{
+					msg = "Not Cool";
+				}
 			}
-			else{
-				msg = "Not Cool";
+			
+			else{//email exists
+				msg = "Exists";
+				System.out.println("From email");
 			}
 			dos.writeUTF(msg);
 		}
