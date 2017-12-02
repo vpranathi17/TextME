d10686f78d5bc606fec51915b254add67d2c476c
vpranathi17 <33180562+vpranathi17@users.noreply.github.com>
2017-12-02 00:30:40 -0500

    Delete server.java

--- server.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-class server {
-    public static void main(String[] args) throws IOException {
-        ServerThread sally = new ServerThread(5000, 100);
-        sally.run();
-    }
-}
