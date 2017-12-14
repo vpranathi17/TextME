b5af06efcd9e68574d2e5c2555dbacbfccda353b
vpranathi17 <33180562+vpranathi17@users.noreply.github.com>
2017-12-13 20:31:32 -0500

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -92,7 +92,7 @@ public class Client extends JFrame{
         chatWin();
     }
 
-    public void chatWin() throws Exception {
+    public void chatWin() throws Exception {//chat application window 
         ImageIcon img = new ImageIcon("/Users/PranathiVasireddy/Desktop/unnamed.png");
         String[] options = new String[] {"SignUp", "SignIn", "Cancel"};
         int reply = JOptionPane.showOptionDialog(null, "Welcome to TextME application", "TextME", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
@@ -110,7 +110,7 @@ public class Client extends JFrame{
         }
     }
 
-    private void signinFrame() throws Exception {
+    private void signinFrame() throws Exception {//sign in frame
         ImageIcon img = new ImageIcon("/Users/PranathiVasireddy/Desktop/unnamed.png");
         JTextField Username = new JTextField(5);
         JPasswordField Password = new JPasswordField(5);
@@ -137,7 +137,7 @@ public class Client extends JFrame{
         }
     }
 
-    private void signupFrame() throws Exception {
+    private void signupFrame() throws Exception {// sign up frame
         ImageIcon img = new ImageIcon("/Users/PranathiVasireddy/Desktop/unnamed.png");
         JTextField Username = new JTextField(5);
         JPasswordField Password = new JPasswordField(5);
@@ -157,13 +157,13 @@ public class Client extends JFrame{
                 "TextME",JOptionPane.OK_CANCEL_OPTION,JOptionPane.PLAIN_MESSAGE,img);
         if (result == 0){
             email = Emailid.getText();
-            if (email.matches("\\[")){
+            if (email.matches(".*[*.")){
                 ImageIcon img1 = new ImageIcon("/Users/PranathiVasireddy/Desktop/red_cross_x_clip_art_7568.jpg");
                 JOptionPane.showMessageDialog(null,"Please enter valid credentials!","TextME",JOptionPane.ERROR_MESSAGE,img1);
                 signupFrame();
             }
             username = Username.getText();
-            if (username.matches("\\[")){
+            if (username.matches(".*[*.")){
                 ImageIcon img1 = new ImageIcon("/Users/PranathiVasireddy/Desktop/red_cross_x_clip_art_7568.jpg");
                 JOptionPane.showMessageDialog(null,"Please enter valid credentials!","TextME",JOptionPane.ERROR_MESSAGE,img1);
                 signupFrame();
@@ -184,15 +184,16 @@ public class Client extends JFrame{
         }
     }
 
-    private void connectServer() throws IOException {
+    private void connectServer() throws IOException {//connects client to server on server ip address and on port 5000
         connection = new Socket(InetAddress.getByName(serverIP), 5000);
     }
 
     private void infoExchange(String username, String password, String email, String info) throws Exception{
-        ObjectInputStream ois = new ObjectInputStream(connection.getInputStream());
+        // exchange of server's public key and client's authentication details along with public key
+    	ObjectInputStream ois = new ObjectInputStream(connection.getInputStream());
         serverPublicKey = (PublicKey) ois.readObject();
         System.out.println(serverPublicKey);
-
+        //time at creation
         String timeString = Long.toString(new Date().getTime());
         System.out.println("timeString.."+timeString);
 
@@ -208,32 +209,32 @@ public class Client extends JFrame{
         }
 
         System.out.println("Hex format : " + sb.toString());
-        byte[] b = new BigInteger(sb.toString()+timeString,16).toByteArray();
-        System.out.println(b);
+        byte[] passby = new BigInteger(sb.toString()+timeString,16).toByteArray();
+ //       System.out.println(passby);
 
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.ENCRYPT_MODE, serverPublicKey);
         byte[] cipherText = cipher.doFinal((username+"["+timeString).getBytes());
-        byte[] cipherText1 = cipher.doFinal(b);
+        byte[] cipherText1 = cipher.doFinal(passby);
         byte[] cipherText2 = cipher.doFinal((email+"["+timeString).getBytes());
         byte[] cipherText3 = cipher.doFinal((info+"["+timeString).getBytes());
 
         DataOutputStream dos1 = new DataOutputStream(connection.getOutputStream());
         dos1.writeInt(cipherText.length);
-        dos1.write(cipherText);
+        dos1.write(cipherText);//encrypted username sent
 
         DataOutputStream dos2 = new DataOutputStream(connection.getOutputStream());
         dos2.writeInt(cipherText1.length);
         System.out.println(cipherText1.length);
-        dos2.write(cipherText1);
+        dos2.write(cipherText1);//encrypted password sent
 
         DataOutputStream dos3 = new DataOutputStream(connection.getOutputStream());
         dos3.writeInt(cipherText2.length);
-        dos3.write(cipherText2);
+        dos3.write(cipherText2);//encrypted email sent
 
         DataOutputStream dos4 = new DataOutputStream(connection.getOutputStream());
         dos4.writeInt(cipherText3.length);
-        dos4.write(cipherText3);
+        dos4.write(cipherText3);//encrypted sign in or sign up information sent
 
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
         keyGen.initialize(1024);
@@ -241,7 +242,7 @@ public class Client extends JFrame{
 
         ObjectOutputStream oos = new ObjectOutputStream(connection.getOutputStream());
         oos.writeObject(clientKey.getPublic());
-        oos.flush();
+        oos.flush();// client's public key is sent
 
 //        System.out.println("Sent details are...");
 //		  System.out.println(cipherText);
@@ -256,32 +257,32 @@ public class Client extends JFrame{
         databaseCheck();
     }
 
-    private void databaseCheck() throws Exception{
+    private void databaseCheck() throws Exception{// receives response from server after database checking
         DataInputStream dis = new DataInputStream(connection.getInputStream());
         String rmsg = dis.readUTF();
-        if (rmsg.equals("OK")){
+        if (rmsg.equals("OK")){//valid user
             ImageIcon img = new ImageIcon("/Users/PranathiVasireddy/Desktop/sign-check-icon.png");
             JOptionPane.showMessageDialog(null," Everything is fine!","TextME",JOptionPane.INFORMATION_MESSAGE,img);
             connections();
         }
         else{
-            if (rmsg.equals("Exists")){
+            if (rmsg.equals("Exists")){// username exists...but sign up 
                 ImageIcon img = new ImageIcon("/Users/PranathiVasireddy/Desktop/images.jpeg");
                 JOptionPane.showMessageDialog(null," User already exists!","TextME",JOptionPane.INFORMATION_MESSAGE,img);
                 chatWin();
             }
             else {
-                if (rmsg.equals("Not Cool")){
+                if (rmsg.equals("Not Cool")){//user name exists.... but password doesn't match
                     ImageIcon img = new ImageIcon("/Users/PranathiVasireddy/Desktop/red_cross_x_clip_art_7568.jpg");
                     JOptionPane.showMessageDialog(null,"Please enter valid credentials!","TextME",JOptionPane.ERROR_MESSAGE,img);
                     chatWin();
                 }
                 else{
-                    if(rmsg.equals("Inserted")){
+                    if(rmsg.equals("Inserted")){// new user created
                         ImageIcon img = new ImageIcon("/Users/PranathiVasireddy/Desktop/svfzpeepadzijwotnacu.gif");
                         JOptionPane.showMessageDialog(null," New user created!","TextME",JOptionPane.INFORMATION_MESSAGE,img);
                         signinFrame();
-                    }else{
+                    }else{// something else went wrong
                         ImageIcon img = new ImageIcon("/Users/PranathiVasireddy/Desktop/red_cross_x_clip_art_7568.jpg");
                         JOptionPane.showMessageDialog(null," Something went wrong!","TextME",JOptionPane.ERROR_MESSAGE,img);
                         chatWin();
@@ -291,7 +292,7 @@ public class Client extends JFrame{
         }
     }
 
-    private void connections() throws Exception{
+    private void connections() throws Exception{// creates connections page
         JTextField user = new JTextField(5);
         JPanel myPanel = new JPanel();
         myPanel.add(new JLabel("Username:"));
@@ -418,38 +419,6 @@ public class Client extends JFrame{
  //       System.out.println("In show up streams");
     }
 
-    public static void encryption(String plainText) throws Exception {
-
-        Signature signatureProvider = null;
-        signatureProvider = Signature.getInstance("SHA256WithRSA");
-        signatureProvider.initSign(clientKey.getPrivate());
-        signatureProvider.update(plainText.getBytes());
-        byte[] signature = signatureProvider.sign();
-
-        String y = signature.toString();
-
-        String timeString = Long.toString(new Date().getTime());
-        System.out.println("timeString.."+timeString);
-        Cipher encCipher = null;
-        encCipher = Cipher.getInstance("RSA");
-        encCipher.init(Cipher.ENCRYPT_MODE, serverPublicKey);
-
-        byte[] encrypted = encCipher.doFinal((plainText+y+timeString).getBytes());
-        System.out.println("encrypted"+encrypted);
-        System.out.println("encrypted.length"+encrypted.length);
-        output.writeInt(encrypted.length);
-        output.write(encrypted);
-        output.flush();
-
-        System.out.println("signature"+signature);
-        System.out.println("length" +signature.length);
-        output.writeInt(signature.length);
-        output.write(signature);
-        output.flush();
-
- //       System.out.println("here");       
-    }
-
     public static void encrypt(String plainText) throws Exception {
 
         Signature signatureProvider = null;
@@ -467,20 +436,18 @@ public class Client extends JFrame{
         encCipher.init(Cipher.ENCRYPT_MODE, key);
 
         byte[] encrypted = encCipher.doFinal((plainText+y+timeString).getBytes());
-        System.out.println("encrypted"+encrypted);
-        System.out.println("encrypted.length"+encrypted.length);
+        System.out.println("Encrypted...."+encrypted);
+        System.out.println("Encrypted.length"+encrypted.length);
         output.writeInt(encrypted.length);
         output.write(encrypted);
         output.flush();
 
-        System.out.println("signature"+signature);
-        System.out.println("length" +signature.length);
+        System.out.println("Signature....."+signature);
+        System.out.println("Length...." +signature.length);
         output.writeInt(signature.length);
         output.write(signature);
         output.flush();
 
-        System.out.println("here");
-        System.out.println(encrypted);
     }
 
     public void whileChatting(String user)throws Exception{
@@ -492,15 +459,14 @@ public class Client extends JFrame{
         do{
             try {
                 int length = input.readInt();
-                System.out.println(length);
                 byte[] cipherText = null;
                 if(length>0) {
                     cipherText = new byte[length];
                     input.readFully(cipherText, 0, cipherText.length); // read the message
                 }
-                System.out.println(cipherText);
+                //System.out.println(cipherText);
                 String timeString = Long.toString(new Date().getTime());
-                System.out.println(timeString);
+                //System.out.println(timeString);
                 decryption(cipherText, timeString,user);
                 System.out.println(message);
                 showMessage(message);
@@ -513,8 +479,8 @@ public class Client extends JFrame{
 
     public void decryption(byte[] cipherText, String timeString, String user)throws Exception {
 
-        System.out.println("start of decryption");
-        System.out.println("mesage before decrytion method in bytes" +cipherText);
+        System.out.println("Start of Decryption");
+        System.out.println("Mesage before decrytion method in bytes..." +cipherText);
         byte[] decipheredMessage = decrypt(cipherText, clientKey.getPrivate());
 
         System.out.println(String.format("The plaintext decripted on server side is : %s", decipheredMessage));
@@ -540,7 +506,7 @@ public class Client extends JFrame{
                 System.out.println("Replayed message");
             }
             else{
-                System.out.println(s2-sub2);
+                //System.out.println(s2-sub2);
                 System.out.println("Original message");
             }
         }
@@ -565,9 +531,7 @@ public class Client extends JFrame{
 
 
         boolean check = publicSignature.verify(signature);
-        System.out.println("check"+check);
-
-        System.out.println("here");
+        System.out.println("check...."+check);
 
         showMessage("\n"+user +"-"+result);
         output.flush();
@@ -611,11 +575,11 @@ public class Client extends JFrame{
     }
 
     public static byte[] decrypt(byte[] encrypted, PrivateKey privateKey) throws Exception {
-        System.out.println("start of decryption method");
+        System.out.println("Start of decrypt method");
         Cipher decriptCipher = Cipher.getInstance("RSA");
         decriptCipher.init(Cipher.DECRYPT_MODE, privateKey);
         byte[] x = decriptCipher.doFinal(encrypted);
-        System.out.println("end of decryption method");
+        System.out.println("End of decrypt method");
         return x;
     }
     private String[] processConnectionInfo(String connectionString){
@@ -628,8 +592,7 @@ public class Client extends JFrame{
     private String getInfofromServer(DataInputStream dis, DataOutputStream dos ) throws IOException {
         String rmsg = null;
         String userMsg = dis.readUTF();//get the user information to connect to the other user.
-        System.out.println(userMsg);
-        System.out.println("Yehah");
+        System.out.println("Client Info..." +userMsg);
         if (userMsg.isEmpty()){
             dos.writeUTF("0");
             rmsg = dis.readUTF();
@@ -640,7 +603,6 @@ public class Client extends JFrame{
         else{
             dos.writeUTF("1");
             rmsg = dis.readUTF();
-            System.out.println("rmsg"+rmsg);
             if (rmsg.equals("1")){// received info of the other client
                 // close all connections
                 //connection.close();
@@ -657,10 +619,9 @@ public class Client extends JFrame{
     }
 
     private String beginChat(String host, String port) throws Exception {
-        System.out.println(port);
+        //System.out.println(port);
         connection = new Socket(InetAddress.getByName(host.substring(1)), Integer.parseInt(port));
         setupStreams();
-        System.out.println("120");
         // connect to the other user
         output.writeUTF(userDetailsTokens[1]);
 
